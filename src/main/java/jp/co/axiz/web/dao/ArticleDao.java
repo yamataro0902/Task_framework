c88d8fed1566b6937a0a6b3ebafad5c6bdc757d0
admin <admin@admin-HP>
2018-07-05 15:18:06 +0900

    あ

---
+++ src/main/java/jp/co/axiz/web/dao/ArticleDao.java
@@ -0,0 +1,33 @@
+package jp.co.axiz.web.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jp.co.axiz.web.entity.Article;
+
+@Repository
+public class ArticleDao {
+
+
+	public List<Article> find(Article article){
+		return null;
+	}
+
+	public Article findById(Integer id) {
+		return null;
+	}
+
+	public void register(Article article) {
+
+	}
+
+	public void update(Article article) {
+
+	}
+
+	public void delete(Integer id) {
+
+	}
+
+}
